c44f6ba78d4d9809ef7ba7990a9eeebfcdff91db
Vlad <youngronaldo2008@gmail.com>
2025-11-30 00:33:00 +0300

    asd

--- app/src/main/java/com/example/topplaygroundcompose/MainActivity.kt
+++ app/src/main/java/com/example/topplaygroundcompose/MainActivity.kt
@@ -5,71 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Devices.PIXEL
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.example.topplaygroundcompose.presentation.navigation.NavGraph
 import com.example.topplaygroundcompose.ui.theme.TopPlaygroundComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Content()
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Content() {
-    TopPlaygroundComposeTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            topBar = {
-                TopAppBar(
-                    modifier = Modifier,
-                    title = { Text(text = stringResource(R.string.hello)) },
-                )
-            }, bottomBar = {
-                Button(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
-                        .padding(bottom = 16.dp), onClick = {}) { }
+            TopPlaygroundComposeTheme {
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
-        ) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(name = "small phone", device = PIXEL, showSystemUi = true)
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TopPlaygroundComposeTheme {
-        Content()
     }
 }
